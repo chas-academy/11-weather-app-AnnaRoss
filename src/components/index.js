b228913c0649c3d98258a33fe21572fa4d6eae1f
Anna Ross <anna.ross@chasacademy.se>
2018-06-07 16:44:36 +0200

    Minor updates of utility files & styles

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,8 @@
-export { default as Forecast } from './Forecast/Forecast';
-export { default as Day } from './Day/Day';
-export { default as HourInterval } from './HourInterval/HourInterval';
+export { default as App } from './app/App';
+export { default as CurrentWeather } from './forecasts/CurrentWeather';
+export { default as Day } from './forecasts/Day';
+export { default as Week } from './forecasts/Week';
+export { default as HourInterval } from './forecasts/HourInterval';
+export { default as Direction } from './common/Direction';
+export { default as Temperature } from './common/Temperature';
+export { default as Time } from './common/Time';
